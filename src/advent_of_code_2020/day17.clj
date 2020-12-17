5f50a9e0f2b45665d5027fab0088235e3d4c0e29
Sebastian Hans <Hans.Sebastian@swm.de>
2020-12-17 14:18:37 +0100

    solve day 17 in another way with greatly increased performance

--- src/advent_of_code_2020/day17.clj
+++ src/advent_of_code_2020/day17.clj
@@ -2,151 +2,92 @@
   (:require [clojure.string :as s]))
 
 (defn- parse-initial-state
-  "Parses the initial state of the pocket dimension into a vector of vectors of
-  vectors of cubes."
+  "Parses the initial state of the pocket dimension into the set of
+  coordinates (2-dimensional vectors) of active cubes."
   [s]
   (->> s
        s/split-lines
-       (map vec)
-       vec
-       vector))
+       (map-indexed (fn [y line]
+                      (map-indexed (fn [x cube] [x y cube]) line)))
+       (mapcat identity)
+       (filter #(= (get % 2) \#))
+       (map #(subvec % 0 2))
+       set))
 
-(defn- active?
-  "Returns true if the cube at the given coordinates is active."
-  [state coordinates]
-  (= (get-in state coordinates) \#))
+(defn- in-dimension
+  "Appends dimensions to the coordinates in state to bring them to n dimensions."
+  [n state]
+  (let [d (- n (.size (first state)))
+         additional-dimensions (vec (repeat d 0))]
+    (set (map #(into % additional-dimensions) state))))
 
-(defn- active-neighbours
-  "Returns the number of active neighbours of the given coordinate in 3 dimensions."
-  [state [z y x]]
-  (apply + (for [dz [-1 0 1]
-                 dy [-1 0 1]
-                 dx [-1 0 1]
-                 :when (not (= 0 dz dy dx))
-                 :let [neighbour-active? (active? state [(+ z dz) (+ y dy) (+ x dx)])
-                       count-neighbour (if neighbour-active? 1 0)]]
-             count-neighbour)))
+(defn- neighbours
+  "Returns all neighbour coordinates for a given coordinate."
+  ([coordinate]
+   (filter #(not= % coordinate) (neighbours [coordinate] 0)))
+  ([coordinates dimension]
+   (if (>= dimension (.size (first coordinates)))
+     coordinates
+     (recur (->> coordinates
+                 (mapcat #(vector (update % dimension dec)
+                                  %
+                                  (update % dimension inc))))
+            (inc dimension)))))
 
-(defn- expand-state
-  "Adds one layer of inactive cubes on all sides of the state in 3 dimensions."
-  [state]
-  (let [z (.size state)
-        y (.size (get state 0))
-        x (.size (get (get state 0) 0))
-        inactive-x (vec (repeat (+ x 2) \.))
-        inactive-y (vec (repeat (+ y 2) inactive-x))]
-    (vec (concat [inactive-y]
-                 (map (fn [y-layer] (vec (concat [inactive-x]
-                                                 (map #(vec (concat [\.] % [\.])) y-layer)
-                                                 [inactive-x]))) state)
-                 [inactive-y]))))
+(defn- print-state
+  "Prints a representation of state."
+  ([state]
+   (->> state
+        (map #(subvec % 2 (.size (first state))))
+        distinct
+        sort
+        (run! #(print-state state %))))
+  ([state layer]
+   (let [min-x (apply min (map #(get % 0) state))
+         max-x (apply max (map #(get % 0) state))
+         min-y (apply min (map #(get % 1) state))
+         max-y (apply max (map #(get % 1) state))]
+     (println "\nLayer:" layer)
+     (doseq [y (range min-y (inc max-y))
+             x (range min-x (inc max-x))
+             :let [c (into [x y] layer)
+                   cube (if (state c) \# \.)]]
+       (print cube)
+       (when (= x max-x)
+         (print "\n"))))))
 
 (defn- apply-conway
-  "Applies the Conway rules to all cubes in 3 dimensions and returns the resulting state."
+  "Applies the Conway rules to all cubes and returns the resulting state."
   [state]
-  (->>
-   (for [z (range (.size state))
-         y (range (.size (get state 0)))
-         x (range (.size (get (get state 0) 0)))
-         :let [coordinates [z y x]
-               active? (active? state coordinates)
-               active-neighbours (active-neighbours state coordinates)
-               will-be-active? (or (and active? (or (= active-neighbours 2)
-                                                    (= active-neighbours 3)))
-                                   (and (not active?) (= active-neighbours 3)))]
-         :when (not= will-be-active? active?)]
-     [coordinates will-be-active?])
-   (filter identity)
-   (reduce (fn [state [coordinates active?]]
-             (assoc-in state coordinates (if active? \# \.)))
-           state)))
+  (let [neighbour-counts (frequencies (mapcat neighbours state))
+        three-neighbours? (fn [[_ n]] (= n 3))
+        two-or-three-neighbours? (fn [[_ n]] (or (= n 2) (= n 3)))
+        coordinate (fn [[c _]] c)
+        newly-activated-cubes (->> neighbour-counts
+                                   (filter three-neighbours?)
+                                   (map coordinate)
+                                   (filter (complement state)))
+        still-active-cubes (->> neighbour-counts
+                                (filter two-or-three-neighbours?)
+                                (map coordinate)
+                                (filter state))]
+    (into (set still-active-cubes) newly-activated-cubes)))
 
-(defn- run-cycle
-  "Runs one cycle in the 3-dimensional pocket dimension."
-  [state]
-  (->> state
-       expand-state
-       apply-conway))
-
-(defn solve-1
-  "Returns the number of active cubes after running the six-cycle boot process."
-  [s]
-  (->> s
-       parse-initial-state
-       (iterate run-cycle)
-       (take 7)
-       last
-       flatten
-       (filter #(= % \#))
-       count))
-
-(defn- active-neighbours-4dim
-  "Returns the number of active neighbours of the given coordinate in 4 dimensions."
-  [state [w z y x]]
-  (apply + (for [dw [-1 0 1]
-                 dz [-1 0 1]
-                 dy [-1 0 1]
-                 dx [-1 0 1]
-                 :when (not (= 0 dw dz dy dx))
-                 :let [neighbour-active? (active? state [(+ w dw) (+ z dz) (+ y dy) (+ x dx)])
-                       count-neighbour (if neighbour-active? 1 0)]]
-             count-neighbour)))
-
-(defn- expand-state-4dim
-  "Adds one layer of inactive cubes on all sides of the state in 4 dimensions."
-  [state]
-  (let [w (.size state)
-        z (.size (get state 0))
-        y (.size (get (get state 0) 0))
-        x (.size (get (get (get state 0) 0) 0))
-        inactive-x (vec (repeat (+ x 2) \.))
-        inactive-y (vec (repeat (+ y 2) inactive-x))
-        inactive-z (vec (repeat (+ z 2) inactive-y))]
-    (vec (concat [inactive-z]
-                 (map expand-state state)
-                 [inactive-z]))))
-
-(defn- apply-conway-4dim
-  "Applies the Conway rules to all cubes in 4 dimensions and returns the resulting state."
-  [state]
-  (->>
-   (for [w (range (.size state))
-         z (range (.size (get state 0)))
-         y (range (.size (get (get state 0) 0)))
-         x (range (.size (get (get (get state 0) 0) 0)))
-         :let [coordinates [w z y x]
-               active? (active? state coordinates)
-               active-neighbours (active-neighbours-4dim state coordinates)
-               will-be-active? (or (and active? (or (= active-neighbours 2)
-                                                    (= active-neighbours 3)))
-                                   (and (not active?) (= active-neighbours 3)))]
-         :when (not= will-be-active? active?)]
-     [coordinates will-be-active?])
-   (filter identity)
-   (reduce (fn [state [coordinates active?]]
-             (assoc-in state coordinates (if active? \# \.)))
-           state)))
-
-(defn- run-cycle-4dim
-  "Runs one cycle in the 4-dimensional pocket dimension."
-  [state]
-  (->> state
-       expand-state-4dim
-       apply-conway-4dim))
-
-(defn solve-2
+(defn- boot-up-and-count
   "Returns the number of active cubes after running the six-cycle boot process in
-  4 dimensions."
-  [s]
+  n dimensions with the initial state described by s."
+  [n s]
   (->> s
        parse-initial-state
-       vector
-       (iterate run-cycle-4dim)
-       (take 7)
+       (in-dimension n)
+       (iterate apply-conway)
+       (take 7) ; because the first one is the initial state
        last
-       flatten
-       (filter #(= % \#))
        count))
+
+(def solve-1 (partial boot-up-and-count 3))
+
+(def solve-2 (partial boot-up-and-count 4))
 
 (def trial-input ".#.
 ..#
