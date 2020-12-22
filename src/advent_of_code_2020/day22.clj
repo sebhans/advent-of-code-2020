d7e872e453768b7652d90916f743b1846952174f
Sebastian Hans <ich@sebastian-hans.de>
2020-12-22 21:30:10 +0100

    solve day 22, part 2

--- src/advent_of_code_2020/day22.clj
+++ src/advent_of_code_2020/day22.clj
@@ -1,8 +1,8 @@
 (ns advent-of-code-2020.day22
   (:require [clojure.string :as s]))
 
-(defn- parse-combat-decks
-  "Parses the two Combat card decks."
+(defn- parse-decks
+  "Parses the two card decks."
   [s]
   (->> s
        (#(s/split % #"\n\n"))
@@ -10,7 +10,7 @@
 
 (defn- play-round-of-combat
   "Plays a round of Combat with the given decks and returns the resulting decks."
-  [[deck1 deck2]]
+  [[^clojure.lang.IPersistentVector deck1 ^clojure.lang.IPersistentVector deck2]]
   (let [card1 (get deck1 0)
         card2 (get deck2 0)
         rest1 (subvec deck1 1)
@@ -22,8 +22,8 @@
 (defn- play-combat
   "Plays a game of Combat with the given decks and returns the winning player's deck."
   [[deck1 deck2 :as decks]]
-  (cond (zero? (.size deck1)) deck2
-        (zero? (.size deck2)) deck1
+  (cond (zero? (.length deck1)) deck2
+        (zero? (.length deck2)) deck1
         :else (recur (play-round-of-combat decks))))
 
 (defn- score-deck
@@ -38,8 +38,55 @@
   "Plays a game of Combat with the decks in s and returns the winning player's score."
   [s]
   (-> s
-      parse-combat-decks
+      parse-decks
       play-combat
+      score-deck))
+
+(declare play-recursive-combat)
+
+(defn- play-round-of-recursive-combat
+  "Plays a round of Recursive Combat with the given decks and returns the
+  resulting decks."
+  [[^clojure.lang.IPersistentVector deck1
+    ^clojure.lang.IPersistentVector deck2 :as decks]]
+  (let [^Long card1 (get deck1 0)
+        ^Long card2 (get deck2 0)
+        ^clojure.lang.IPersistentVector rest1 (subvec deck1 1)
+        ^clojure.lang.IPersistentVector rest2 (subvec deck2 1)]
+    (let [^Long winner (cond (and (>= (.length rest1) card1)
+                                  (>= (.length rest2) card2))
+                             (get (play-recursive-combat [(subvec rest1 0 card1)
+                                                          (subvec rest2 0 card2)]) 0)
+                             (> card1 card2)
+                             1
+                             :else
+                             2)]
+      (if (= winner 1)
+        [(conj rest1 card1 card2) rest2]
+        [rest1 (conj rest2 card2 card1)]))))
+
+(defn- play-recursive-combat
+  "Plays a game of Recursive Combat with the given decks and returns a pair
+  [winner deck] where winner is 1 or 2 and indicates which player has won,
+  and deck is their winning deck."
+  ([^clojure.lang.IPersistentVector decks]
+   (play-recursive-combat decks #{}))
+  ([[^clojure.lang.IPersistentVector deck1
+     ^clojure.lang.IPersistentVector deck2 :as decks]
+    ^clojure.lang.IPersistentSet history]
+   (cond (history decks) [1 deck1]
+         (zero? (.length deck1)) [2 deck2]
+         (zero? (.length deck2)) [1 deck1]
+         :else (recur (play-round-of-recursive-combat decks) (conj history decks)))))
+
+(defn solve-2
+  "Plays a game of Recursive Combat with the decks in s and returns the winning
+  player's score."
+  [s]
+  (-> s
+      parse-decks
+      play-recursive-combat
+      second
       score-deck))
 
 (def trial-input "Player 1:
